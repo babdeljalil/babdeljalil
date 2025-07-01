223f9a89c29ac0890cc4a85f7fd8754a4413ea40
shadowroot <158838565+SHADOWROOTmy@users.noreply.github.com>
2025-07-02 00:55:31 +0100

    Update Hero image to use local profile photo
    Replaces the external image in the Hero component with a local profile image, updates the import, and adjusts image styling and dimensions. Also renames the profile image file for consistency.

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import profile from "../../../public/babdeljalil-profile.jpeg";
+
 export default function Hero() {
   return (
     <section className="flex max-md:flex-col w-full py-20">
@@ -17,11 +19,11 @@ export default function Hero() {
       </div>
       <div className="w-1/2 max-md:w-full px-16 max-md:py-10 max-md:px-6">
         <Image
-          src="https://images.pexels.com/photos/3694884/pexels-photo-3694884.jpeg?auto=compress&cs=tinysrgb&w=800"
+          src={profile}
           alt=""
-          width={500}
-          height={500}
-          className="object-cover object-top"
+          width={250}
+          height={250}
+          className="object-cover object-top h-72 w-72 rounded-md"
         />
       </div>
     </section>

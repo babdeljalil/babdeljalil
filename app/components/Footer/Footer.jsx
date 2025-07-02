5af9b8d761e279dd1e410c639ec39011591c42c0
shadowroot <158838565+SHADOWROOTmy@users.noreply.github.com>
2025-07-02 01:23:04 +0100

    Update Footer with personal info and social links
    Replaces the abstract footer text with a personal introduction for Berhil Abdeljalil, updates the copyright to 'BAbdeljalil', and wraps social media icons with links to the relevant profiles.

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -21,45 +21,57 @@ export default function Footer() {
         </div>
         <div className="flex flex-col justify-between">
           <p className="w-[70%] text-lg font-bold max-md:my-6">
-            Orchestras composed entirely of sneezing dust particles perform
-            symphonies in the spaces between your blinking patterns.
+            Hi, I’m Berhil Abdeljalil, a frontend developer from Morocco 🇲🇦. I
+            make symphonies out of sneezing dust particles in the space between
+            your blinks.
           </p>{" "}
           <div className="w-full flex justify-end">
             <div className="flex items-center">
-              <Image
-                src="icons/github.svg"
-                alt=""
-                width={500}
-                height={500}
-                className="h-7 mx-1.5 w-fit"
-              />
-              <Image
-                src="icons/x.svg"
-                alt=""
-                width={500}
-                height={500}
-                className="h-7 mx-1.5 w-fit"
-              />
-              <Image
-                src="icons/instagram.svg"
-                alt=""
-                width={500}
-                height={500}
-                className="h-7 mx-1.5 w-fit"
-              />
-              <Image
-                src="icons/linkedin.svg"
-                alt=""
-                width={500}
-                height={500}
-                className="h-7 mx-1.5 w-fit"
-              />
+              <a href="https://github.com/babdeljalil" target="_blank">
+                <Image
+                  src="icons/github.svg"
+                  alt=""
+                  width={500}
+                  height={500}
+                  className="h-7 mx-1.5 w-fit"
+                />
+              </a>
+              <a href="https://x.com/babdeljalil17" target="_blank">
+                <Image
+                  src="icons/x.svg"
+                  alt=""
+                  width={500}
+                  height={500}
+                  className="h-7 mx-1.5 w-fit"
+                />
+              </a>
+              <a href="https://instagram.com/babdeljalil.my" target="_blank">
+                <Image
+                  src="icons/instagram.svg"
+                  alt=""
+                  width={500}
+                  height={500}
+                  className="h-7 mx-1.5 w-fit"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/abdeljalil-berhil-302a92367/"
+                target="_blank"
+              >
+                <Image
+                  src="icons/linkedin.svg"
+                  alt=""
+                  width={500}
+                  height={500}
+                  className="h-7 mx-1.5 w-fit"
+                />
+              </a>
             </div>
           </div>
         </div>
       </div>
       <div className="w-full justify-center items-center flex font-bold my-4">
-        <span>© 2025 Zaggonaut. All rights reserved.</span>
+        <span>© 2025 BAbdeljalil. All rights reserved.</span>
       </div>
     </footer>
   );

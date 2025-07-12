af71a3f17a4334d3bc669bbe2a01bc854e690217
babdeljalil <158838565+babdeljalil@users.noreply.github.com>
2025-07-12 12:20:49 +0100

    Remove MDX support and related dependencies
    Deleted the Blogs page and removed MDX configuration from next.config.ts. Also cleaned up package.json by removing MDX-related dependencies and Nextra packages.

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,9 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
-import nextMdx from "@next/mdx";
-
-const withMdx = nextMdx({
-  // By default only the `.mdx` extension is supported.
-  extension: /\.mdx?$/,
-  options: {
-    /* otherOptions… */
-  },
-});
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
     domains: ["images.pexels.com"],
   },
 };
 
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [["rehype-katex", { strict: true, throwOnError: true }]],
-  },
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
